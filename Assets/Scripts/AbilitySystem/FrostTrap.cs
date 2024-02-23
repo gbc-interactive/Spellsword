cff5c82a5061706e5f7c857e8c9d01c2430319fc
Yooshley <101507077@georgebrown.ca>
2024-02-22 20:50:44 -0500

    PostProcessing and tree variation update

--- Assets/Scripts/AbilitySystem/FrostTrap.cs
+++ Assets/Scripts/AbilitySystem/FrostTrap.cs
@@ -2,7 +2,6 @@ using Spellsword;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class FrostTrap : AbilityBase
