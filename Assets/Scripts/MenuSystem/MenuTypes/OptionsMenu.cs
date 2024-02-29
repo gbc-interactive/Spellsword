8241b5241c861f0ececb2d73a401e2bd651d06b0
Yooshley <101507077@georgebrown.ca>
2024-02-29 18:12:26 -0500

    Resolved options menu

--- Assets/Scripts/MenuSystem/MenuTypes/OptionsMenu.cs
+++ Assets/Scripts/MenuSystem/MenuTypes/OptionsMenu.cs
@@ -1,15 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace Spellsword
 {
-    public class OptionsMenu : MonoBehaviour,IMenu
+    public class OptionsMenu : MonoBehaviour, IMenu
     {
         public GameObject _optionsMenu;
-        void Start(){
+        [SerializeField] private Button _quitButton;
+        [SerializeField] private AudioMixer _audioMixer;
+        [SerializeField] private TMP_Dropdown _resolutionDropdown;
+        public Resolution[] _resolutions;
+
+        void Start()
+        {
             Disable();
+            _resolutions = Screen.resolutions;
+            _resolutionDropdown.ClearOptions();
+            List<String> options = new List<String>();
+            int currentIndex = 0;
+            for (int i = 0; i < _resolutions.Length; i++)
+            {
+                string option = _resolutions[i].width + " x " + _resolutions[i].height;
+                options.Add(option);
+
+                if (_resolutions[i].width == Screen.width && _resolutions[i].height == Screen.height)
+                {
+                    currentIndex = i;
+                }
+            }
+            _resolutionDropdown.AddOptions(options);
+            _resolutionDropdown.value = currentIndex;
+            _resolutionDropdown.RefreshShownValue();
         }
         public void Enable()
         {
@@ -27,7 +53,28 @@ namespace Spellsword
                 MenuManager.Instance.ChangeMenu(FindObjectOfType<JournalMenu>());
             }
         }
+        public void SetVolume(float volume)
+        {
+            _audioMixer.SetFloat("volume", volume);
+        }
+        public void SetRsolution(int index)
+        {
+            Resolution resolution = _resolutions[index];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 
-        
+        }
+        public void SetFullscreen(bool isFullscreen)
+        {
+            Screen.fullScreen = isFullscreen;
+        }
+        public void SetQuality(int index)
+        {
+            QualitySettings.SetQualityLevel(index);
+        }
+        public void QuitGame()
+        {
+            //UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
     }
 }
